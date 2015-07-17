5fec6960f32603a21615332a0179b0e1405e5cd7
Rajat Ashok <rajatashok@gmail.com>
2015-07-17 14:11:00 -0400

    Retrieved field info without filtering text

--- asr2json.js
+++ asr2json.js
@@ -4,14 +4,14 @@ fs.readFile('asr.json', 'utf8', function(err, data) {
     if (err) {
         throw err;
     }
-    var json = JSON.parse(data);
+    json = JSON.parse(data);
 
     var flag = 0;
     var sectionFlag = 0;
     for( var i = 0; i<json.formImage["Pages"].length; i++) {
 //    for( var i = 0; i<2; i++) { // For each page
         var texts = json.formImage["Pages"][i]["Texts"];
-        for( var j = 0; j<texts.length; j++) {  // For text in the page
+        for( j = 0; j<texts.length; j++) {  // For text in the page
             var R = texts[j]["R"];
             for( var k = 0; k<R.length; k++) {  // For each line
                 var T = R[k]["T"];
@@ -69,117 +69,181 @@ fs.readFile('asr.json', 'utf8', function(err, data) {
                     console.log("Field Number= " + fieldNumber[0]);
 
                     // Title
+                    texts = json.formImage["Pages"][i]["Texts"];
                     R = texts[++j]["R"];
                     var title = R[0]["T"];
                     title = title.split("%20");
                     console.log("Title= " + title[0]);
 
                     // Name
+                    texts = json.formImage["Pages"][i]["Texts"];
                     R = texts[++j]["R"];
                     var name = R[k]["T"];
                     name = name.split("%20");
                     console.log("Name= " + name.join(" "));
 
                     // Description
+                    texts = json.formImage["Pages"][i]["Texts"];
+                    console.log("text= " + texts[j]["R"][0]["T"]);
                     R = texts[++j]["R"];
-                    var note = R[k]["T"].split("%20");
-                    var description = "";
-                    while ( note[0]!= "NOTE" ) {
-                        description = description + " " + R[k]["T"];
-                        description = description.split("%20");
-                        description = description.join(" ");
-                        R = texts[++j]["R"];
-                        note = R[k]["T"].split("%20");
-                    }
-                    console.log("\nDescription= " + description);
+                    var breakString = [ "NOTE", "VALID ENTRIES", "USAGE", "DATA CHARACTERISTICS", "EXAMPLE", "EXAMPLES" ];
+                    var breakValue = "";
+                    var returnValue = getFieldInfo(R, texts, json, i, j, k, breakString, breakValue, fieldNumber[0]);
+                    // Copy back returned values
+                    i = returnValue[0];
+                    j = returnValue[1];
+                    k = returnValue[2];
+                    breakValue = returnValue[3];
+                    var description = returnValue[4];
+                    console.log("Description= " + description);
 
                     // Field Notes
-                    R = texts[++j]["R"];
-                    var validEntries = R[k]["T"].split("%3A");
-                    validEntries = validEntries[0].split("%20");
-                    validEntries = validEntries.join(" ");
-                    var fieldNotes = "";
-                    while ( validEntries!= "VALID ENTRIES" ) {
-                        fieldNotes = fieldNotes + " " + R[k]["T"];
-                        fieldNotes = fieldNotes.split("%20");
-                        fieldNotes = fieldNotes.join(" ");
+                    if ( breakValue == "NOTE") {
+                        texts = json.formImage["Pages"][i]["Texts"];
                         R = texts[++j]["R"];
-
-                        validEntries = R[k]["T"].split("%3A");
-                        validEntries = validEntries[0].split("%20");
-                        validEntries = validEntries.join(" ");
+                        breakString = [ "VALID ENTRIES", "USAGE", "DATA CHARACTERISTICS", "EXAMPLE", "EXAMPLES" ];
+                        returnValue = getFieldInfo(R, texts, json, i, j, k, breakString, breakValue, fieldNumber[0]);
+                        // Copy back returned values
+                        i = returnValue[0];
+                        j = returnValue[1];
+                        k = returnValue[2];
+                        breakValue = returnValue[3];
+                        var fieldNotes = returnValue[4];
+                        console.log("\nField Notes= " + fieldNotes);
                     }
-                    fieldNotes = fieldNotes.split("NOTE");
-                    fieldNotes = fieldNotes.join("\n");
-                    console.log("\nField Notes= " + fieldNotes);
 
                     // Valid Entries
-                    R = texts[++j]["R"];
-                    var note = R[k]["T"].split("%20");
-                    var validEntries = "";
-                    while ( note[0]!= "NOTE" ) {
-                        validEntries = validEntries + "\n" + R[k]["T"];
-                        validEntries = validEntries.split("%20");
-                        validEntries = validEntries.join(" ");
+                    if ( breakValue == "VALID ENTRIES") {
+                        texts = json.formImage["Pages"][i]["Texts"];
                         R = texts[++j]["R"];
-                        note = R[k]["T"].split("%20");
+                        breakString = [ "NOTE", "USAGE", "DATA CHARACTERISTICS", "EXAMPLE", "EXAMPLES" ];
+                        returnValue = getFieldInfo(R, texts, json, i, j, k, breakString, breakValue, fieldNumber[0]);
+                        // Copy back returned values
+                        i = returnValue[0];
+                        j = returnValue[1];
+                        k = returnValue[2];
+                        breakValue = returnValue[3];
+                        var validEntries = returnValue[4];
+                        console.log("\nvalidEntries= " + validEntries);
                     }
-                    console.log("\nvalidEntries= " + validEntries);
 
                     // Valid Entry Notes
-                    R = texts[++j]["R"];
-                    var usage = R[k]["T"].split("%3A");
-                    var validEntryNotes = "";
-                    while ( usage[0]!= "USAGE" ) {
-                        validEntryNotes = validEntryNotes + " " + R[k]["T"];
-                        validEntryNotes = validEntryNotes.split("%20");
-                        validEntryNotes = validEntryNotes.join(" ");
-                        if ( j + 1 == texts.length ) {
-                            j = 0;
-                            i++;
-//                            console.log("i= " + i);
-//                            console.log("json.formImage[Pages].length= " + json.formImage["Pages"].length);
-                            texts = json.formImage["Pages"][i]["Texts"];
-                        }
-
+                    if ( breakValue == "NOTE") {
+                        texts = json.formImage["Pages"][i]["Texts"];
                         R = texts[++j]["R"];
-                        usage = R[k]["T"].split("%3A");
-//                        console.log("usage= " + usage[0]);
+                        breakString = [ "USAGE", "DATA CHARACTERISTICS", "EXAMPLE", "EXAMPLES" ];
+                        returnValue = getFieldInfo(R, texts, json, i, j, k, breakString, breakValue, fieldNumber[0]);
+                        // Copy back returned values
+                        i = returnValue[0];
+                        j = returnValue[1];
+                        k = returnValue[2];
+                        breakValue = returnValue[3];
+                        var validEntryNotes = returnValue[4];
+                        console.log("\nvalidEntryNotes= " + validEntryNotes);
                     }
-                    console.log("\nvalidEntryNotes= " + validEntryNotes);
 
                     // Usage
-                    R = texts[++j]["R"];
-                    var dataCharacteristics = R[k]["T"].split("%3A");
-                    dataCharacteristics = dataCharacteristics[0].split("%20");
-                    dataCharacteristics = dataCharacteristics.join(" ");
-                    var usage = "";
-                    while ( dataCharacteristics!= "DATA CHARACTERISTICS" ) {
-                        usage = usage + " " + R[k]["T"];
-                        usage = usage.split("%20");
-                        usage = usage.join(" ");
+                    console.log("BreakValue= " + breakValue);
+                    if ( breakValue == "USAGE") {
+                        texts = json.formImage["Pages"][i]["Texts"];
                         R = texts[++j]["R"];
-                        dataCharacteristics = R[k]["T"].split("%3A");
-                        dataCharacteristics = dataCharacteristics[0].split("%20");
-                        dataCharacteristics = dataCharacteristics.join(" ");
+                        breakString = [ "NOTE", "DATA CHARACTERISTICS", "EXAMPLE", "EXAMPLES" ];
+                        returnValue = getFieldInfo(R, texts, json, i, j, k, breakString, breakValue, fieldNumber[0]);
+                        // Copy back returned values
+                        i = returnValue[0];
+                        j = returnValue[1];
+                        k = returnValue[2];
+                        breakValue = returnValue[3];
+                        var usage = returnValue[4];
+                        console.log("\nusage= " + usage);
                     }
-                    console.log("usage= " + usage);
+
+                    // Usage Notes
+                    if ( breakValue == "NOTE") {
+                        texts = json.formImage["Pages"][i]["Texts"];
+                        R = texts[++j]["R"];
+                        breakString = [ "DATA CHARACTERISTICS", "EXAMPLE", "EXAMPLES" ];
+                        returnValue = getFieldInfo(R, texts, json, i, j, k, breakString, breakValue, fieldNumber[0]);
+                        // Copy back returned values
+                        i = returnValue[0];
+                        j = returnValue[1];
+                        k = returnValue[2];
+                        breakValue = returnValue[3];
+                        var usageNotes = returnValue[4];
+                        console.log("\nusageNotes= " + usageNotes);
+                    }
+
 
                     // Data Characteristics
-                    R = texts[++j]["R"];
-                    dataCharacteristics = R[k]["T"].split("%20");
-                    console.log("fieldLength = " + dataCharacteristics[0]);
-                    if ( dataCharacteristics[1].indexOf("alpha") > -1 && dataCharacteristics[1].indexOf("numeric") > -1) {
-                        console.log("characteristics = Alpha");
+                    if ( breakValue == "DATA CHARACTERISTICS") {
+                        texts = json.formImage["Pages"][i]["Texts"];
+                        R = texts[++j]["R"];
+                        breakString = [ "EXAMPLE" ];
+                        returnValue = getFieldInfo(R, texts, json, i, j, k, breakString, breakValue, fieldNumber[0]);
+                        // Copy back returned values
+                        i = returnValue[0];
+                        j = returnValue[1];
+                        k = returnValue[2];
+                        breakValue = returnValue[3];
+                        var dataCharacteristics = returnValue[4];
+                        console.log("\nDataCharacteristics= " + dataCharacteristics);
                     }
-                    else if ( dataCharacteristics[1].indexOf("alpha") > -1 ) {
-                        console.log("characteristics = Alpha");
-                    }
-                    else if ( dataCharacteristics[1].indexOf("numeric") > -1) {
-                        console.log("characteristics = Numeric");
+
+                    // EXAMPLE OR EXAMPLES
+                    if ( breakValue == "EXAMPLE" || breakValue == "EXAMPLES") {
+                        j = texts.length;
+                        break;
                     }
                 }
             }
         }
     }
 });
+
+function getFieldInfo(R, texts, json, i, j, k, breakString, breakValue, fieldNumber) {
+    var line = R[k]["T"].split("%20");
+    var fieldInfo = "";
+    var fieldFlag = 0;
+    while (true) {
+        fieldInfo = fieldInfo + " " + R[k]["T"];
+        fieldInfo = fieldInfo.split("%20");
+        fieldInfo = fieldInfo.join(" ");
+        // Move to next page if last line
+        if ( j + 1 == texts.length ) {
+            console.log("\nEND OF PAGE\n" + fieldInfo);
+            i++;
+            texts = json.formImage["Pages"][i]["Texts"];
+            var checkField = texts[5]["R"][0]["T"].split(".");
+
+
+            if ( checkField[0] == fieldNumber ) {
+                console.log("FIELD NUMBER SAME " + checkField + "         " + fieldNumber);
+                j = 6;
+            }
+            else {
+                break;
+            }
+        }
+        R = texts[++j]["R"];
+        line = R[k]["T"].split("%20");
+        line = line.join(" ");
+
+//        console.log("FieldInfo= " + fieldInfo);
+//        console.log("Line= " + line);
+
+        for ( var index=0; index<breakString.length; index++ ) {
+//            console.log("In loop");
+//            console.log("Line= " + line);
+            if ( line.indexOf( breakString[index] ) > -1 ) {
+                breakValue = breakString[index];
+                console.log("BreakValue= " + breakValue);
+                fieldFlag = 1;
+                break;
+            }
+        }
+        if ( fieldFlag == 1 ) {
+            break;
+        }
+    }
+    return([ i, j, k, breakValue, fieldInfo ]);
+}
