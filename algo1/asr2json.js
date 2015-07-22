f7381dd0bae2113da22ba2dbe477fc0169e0de5e
Willison Lopes <wjl4193@g.rit.edu>
2015-07-22 15:13:16 -0400

    Retrieve definitions with not completely filtered text

--- algo1/asr2json.js
+++ algo1/asr2json.js
@@ -1,127 +1,201 @@
 var fs = require('fs');
+var field = {};
 
 fs.readFile('asr.json', 'utf8', function(err, data) {
     if (err) {
         throw err;
     }
     var json = JSON.parse(data);
-    var previous = "version";
-    var number = 0;
-    var notesflag = 0;  //flag for notes. 1. field notes, 2. Valid entry notes, 3. Usage notes, 4. Example notes.
-    var notenumber = 0; //The note number
-    var section_flag = 0;        // flag to indicate the section for the field ( 0 = admin, 1 = bill, 2 = contact)
+    field.fieldNumber = 0;
     for( var i = 0; i<json.formImage["Pages"].length; i++) {
         var texts = json.formImage["Pages"][i]["Texts"];
-        for( var j = 0; j<texts.length; j++) {
-            var R = texts[j]["R"];
-            for( var k = 0; k<R.length; k++) {
-                var value = R[k]["T"];
-                var TS = R[k]["TS"];
-//                console.log(j+ " " +TS[2] + " " + value);
-
-
-                var bold = TS[2];
-                value = decodeURI(value);
-//                console.log(j+ " " +bold + " " + value);
-                if(bold === 1){
-                //bold and not a keyword
-                 if(isNaN(value)){
-                  //This is a new form so create a new folder for the form
-                  if(value.indexOf("FORM") > -1){           //keyword form
-//                    fs.mkdirSync(value);
-//                    console.log(" ASR" + value);
-                    previous = "form";
-                  }else if(value.indexOf("SECTION") > -1){   //keyword section
-                    //Checks to find the section for the fields
-                    if(value.indexOf("ADMINISTRATIVE") > -1){
-                    section_flag = 0;
-                    console.log("section : ADMIN");
-                    }else if(value.indexOf("BILL") > -1){
-                    section_flag = 1;
-                    console.log("section : BILL");
-                    }else if(value.indexOf("CONTACT") > -1){
-                    section_flag = 2;
-                    console.log("section : CONTACT");
-                    }
-                    previous = "SECTION";
-                  }else if(value.indexOf("NOTE") > -1){
-//                    console.log(notesflag + " " +value);
-                    notenumber = (value.substring(value.indexOf(" "), value.indexOf("%3A")));
-                    previous = "NOTES";
-                  }else if(value.indexOf("VALID ENTRIES") > -1){
-                    notesflag = 2;
-                    previous = "ENTRIES";
-                  }else if(value.indexOf("USAGE") > -1){
-                    previous = "USE";
-                    notesflag = 3;
-                  }else if(value.indexOf("DATA CHARACTERISTICS") > -1){
-                    previous = "DATA";
-                  }else if(value.indexOf("EXAMPLE") > -1){
-                    previous = "EX";
-                    notesflag = 4;
-                  }else if(previous === "ENTRIES"){
-//                        console.log("Valid Entry "+number + " " +value);
-                  }else if(value.indexOf("-") > -1){
-                  //check for fields
-//                        if(previous === "SECTION")
-//                       var words = value.split(" ");
-//                       if(words[words.length-1] === "-"){
-//                        console.log("title:" + value);
-//                       }
-//                        console.log("field number "+previous);
-                        console.log("title: " +value + "\n  field number: " +number.slice(0,-1));
-                        previous = "TITLE";
-                        notesflag = 1;
-                    }
-                 }else{
-                    number = value;
-                    }
-                 }else if(bold === 0){
-//                 console.log(previous);
-                    if(previous === "TITLE"){
-                      console.log("name: " +value);
-                        previous = "NAME";
-                    }else if(previous === "NAME" || previous === "DESCRIPTION"){
-                      console.log("description: " +value);
-                        previous = "DESCRIPTION";
-                        notesflag = 1;
-                    }else if(previous === "NOTES"){
-                      console.log("NOTE: "+notenumber +" " +value);
-                    }else if(previous === "ENTRIES"){
-                        console.log("Valid Entries " +value);
-                    }else if(previous === "USE"){
-                        console.log("USAGE: " +value);
-                    }else if(previous === "DATA"){
-                        console.log("DATA: " +value);
-                    }else if(previous === "EX"){
-                        console.log("EX: " +value);
-                    }
-                 }
-            }
-        }
-        content = " ";
+        processPage(texts);
         previous = "none";
     }
 });
-//});
 
+//function to process the page and fine the definitions in the json file page by page
+function processPage(texts){
+    var previous = " ";
+    var content = " ";
+    var number = 0;
+    var notesflag = 0;  //flag for notes. 1. field notes, 2. Valid entry notes, 3. Usage notes, 4. Example notes.
+//    var notenumber = 0; //The note number
+    var section_flag = 0;        // flag to indicate the section for the field ( 0 = admin, 1 = bill, 2 = contact)
+    for( var j = 0; j<texts.length; j++) {
+        var R = texts[j]["R"];
+        for( var k = 0; k<R.length; k++) {
+            var value = R[k]["T"];
+            var TS = R[k]["TS"];
+//            console.log(j+ " " +TS[2] + " " + value);
 
-//function convertasr(bold,value,callback){
-//    //some work
-////    getval(function (err, results){
-//       if(err){
-//        return callback(new Error("error"));
-//       }else{
-//        callback(5);
-//       }
-//    }
+            var bold = TS[2];
+            value = decodeURI(value);
+//                console.log(j+ " " +bold + " " + value);
+           if(bold === 1){
+            //bold and not a keyword
+            if(isNaN(value)){
+            //call to split data characteristics
+                getFormat(previous,content);
+                printContent(previous,content);
+                content = " ";
+                if(value.indexOf("FORM") > -1){           //keyword form
+//                    fs.mkdirSync(value);
+//                    console.log(" ASR" + value);
+                    previous = "form";
+                    }else if(value.indexOf("SECTION") > -1){   //keyword section
+        //                    Checks to find the section for the fields
+                        if(value.indexOf("ADMINISTRATIVE") > -1){
+                            section_flag = 0;
+                            field.section = "ADMIN";
+                            content = content + "ADMIN";
+//                            console.log("section : ADMIN");
+                        }else if(value.indexOf("BILL") > -1){
+                            section_flag = 1;
+                            field.section = "BILL";
+                            content = content + "BILL";
+//                            console.log("section : BILL");
+                        }else if(value.indexOf("CONTACT") > -1){
+                            section_flag = 2;
+                            field.section = "CONTACT";
+                            content = content + "CONTACT";
+//                            console.log("section : CONTACT");
+                        }
+                        previous = "SECTION";
+                    }else if(value.indexOf("NOTE") > -1){
+            //                    console.log(notesflag + " " +value);
+//                        notenumber = (value.substring(value.indexOf(" "), value.indexOf("%3A")));
+                        previous = "NOTES";
+                    }else if(value.indexOf("VALID ENTRIES") > -1){
+                        notesflag = 2;
+                        previous = "VALID ENTRIES";
+                    }else if(value.indexOf("USAGE") > -1){
+                        previous = "USAGE";
+                        notesflag = 3;
+                    }else if(value.indexOf("DATA CHARACTERISTICS") > -1){
+                        previous = "DATA";
+                    }else if(value.indexOf("EXAMPLE") > -1 || value.indexOf("EXAMPLES") > -1){
+                        previous = "EXAMPLE";
+                        notesflag = 4;
+                    }else if(previous === "VALID ENTRIES"){
+                        content = content + value;
+//                        console.log(content);
+//                        console.log("Valid Entry "+number + " " +value);
+                    }else if(value.indexOf("-") > -1){
+//                        check for fields
+                        previous = "TITLE";
+                        value = getTitle(value);
+                        printContent(previous,value);
+                        printContent("fieldnumber",number);
+                        notesflag = 1;
+                    }
+            }else{
+                if(previous === "VALID ENTRIES"){
+                    //add to valid entries
+                }
+                number = value;
+            }
+
+           }else if(bold === 0){
+//                 console.log(previous);
+                if(previous === "TITLE"){
+//                    console.log("NAME: " +value);
+                    previous = "NAME";
+                    printContent(previous,value);
+                }else if(previous === "NAME" || previous === "DEFINITION"){
+                    content = content + value;
+//                    console.log("definition: " +value);
+                    previous = "DEFINITION";
+//                    content = content + value;
+                    notesflag = 1;
+                }else if(previous === "NOTES"){
+                    content = content + value;
+//                    console.log(previous +"NOTE: "+notenumber +" " +value);
+                }else if(previous === "VALID ENTRIES"){
+                    content = content + value;
+//                    console.log(previous +"Valid Entries " +value);
+                }else if(previous === "USAGE"){
+                    content = content + value;
+//                    console.log(previous +"USAGE: " +value);
+                }else if(previous === "DATA"){
+                    content = content + value;
+//                    console.log(previous +"DATA: " +value);
+                }else if(previous === "EXAMPLE"){
+                    content = content + value;
+//                    console.log(previous +"EX: " +value);
+                }
+            }
+        }
+    }
+    printContent(previous,content);
+}
+
+//format the description based on the tag
+function getFormat(previous,content){
+    if(previous === "DATA"){
+        getLength(content);
+    }else if(previous === "VALID ENTRIES"){
+//        formatValidEntries(content);
+    }
+}
+
+//function to convert valid entries to value/description format
+//function formatValidEntries(content){
 //}
-//
-//convertasr(1,3,function(err,va){
-//   console.log(va);
-//});
 
 
+//function to get field length and characteristics
+function getLength(content){
+    var values = content.trim();
+    values = values.split(" ");
+    console.log("fieldLength: " + values[0]);
+    if(values[1] === "alpha"){
+        console.log("characteristics: Alpha");
+    }else if(values[1] === "numeric"){
+        console.log("characteristics: Numeric");
+    }else if(values[1].indexOf("%2F") > -1){
+        console.log("characteristics: AlphaNumeric");
+    }
+}
+
+//extract the title, field number
+function getTitle(content){
+    var title = " ";
+    console.log(content);
+    if(content.indexOf(".") > -1){
+        var values = content.replace(/ /g,'');
+        //check for field number, if it exists then ignore
+        title = title + values;
+    }else{
+//        console.log(content);
+        return content;
+    }
+//    console.log(title);
+    return title.substring(0,title.length-1);
+}
+
+//print the definition read in the json file
+function printContent(previous,content){
+    if(previous === " " && content === " "){
+    }else if(content != " "){
+        content = convertToText(content);
+        console.log(previous + ": " + content);
+    }
+}
+
+//function to convert the special characters in text
+//returns the converted text
+function convertToText(content){
+    var specialCharacters= [ "T\n", "\n", "%20", "%26", " b y ", " o f ", " w ithin ", " NOTE ", "%2C", "%E2%80%9C", "%E2%80%9D", "%E2%80%99", "%2F", "%3A ", "2%3A ", "3%3A ", "4%3A ", "5%3A ", "6%3A ", "7%3A ", " %3D " ];
+    var joinText = [ "T", "  ", " ", " & " , " by ", " of ", " within ", "", ",", "\"", "\"", "\'", "/", ":\n", "\n", "\n", "\n", "\n", "\n", "\n", "=" ];
+       for( var index = 0; index < specialCharacters.length; index++ ) {
+           content = content.split(specialCharacters[index]);
+           content = content.join(joinText[index]);
+       }
+    return content;
+}
+
+//function to write the definitions in a separate file
 //fs.writeFile('myfile.json', 'Hey there!', 'utf8', function(err){
 //    if(err){
 //        return console.log(err);
