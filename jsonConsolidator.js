3a240c2be1d8723f25fb5a23d32c16d9e29cc1c5
Rajat Ashok <rajatashok@gmail.com>
2015-07-30 10:33:29 -0400

    Commit before refactoring code

--- jsonConsolidator.js
+++ jsonConsolidator.js
@@ -5,7 +5,6 @@ fs.readFile('asrpdf.json', 'utf8', function(err, data) {
         throw err;
     }
     var json = JSON.parse(data);
-//    for( var i = 0; i<json.formImage["Pages"].length; i++) {
     for( var i = 0; i<5; i++) {
         var texts = json.formImage["Pages"][i]["Texts"];
         for( var j = 0; j<texts.length; j++) {
@@ -14,17 +13,9 @@ fs.readFile('asrpdf.json', 'utf8', function(err, data) {
 
             var tempj = j;
             while ( j+1 != texts.length) {
-//                console.log("texts= " + texts[j+1] );
                 if ( texts[j+1]["y"] == texts[tempj]["y"] ) {
-//                    console.log(json.formImage["Pages"][i]["Texts"][j]["R"]);
                     json.formImage["Pages"][i]["Texts"][tempj]["R"][0]["T"] +=  json.formImage["Pages"][i]["Texts"][++j]["R"][0]["T"];
                     delete json.formImage["Pages"][i]["Texts"][j];
-//                    j++; // Increment again because object deleted
-//                    R = texts[++j]["R"];
-//                    console.log(R);
-//                    T = T + R[0]["T"];
-
-//                    console.log("After= " + T);
                 }
                 else {
                     break;
@@ -33,7 +24,6 @@ fs.readFile('asrpdf.json', 'utf8', function(err, data) {
             console.log(T);
         }
     }
-
     fs.writeFile("newjsonfile.json", JSON.stringify(json, null, 4), function(err) {
         if (err) {
             return console.log(err);
